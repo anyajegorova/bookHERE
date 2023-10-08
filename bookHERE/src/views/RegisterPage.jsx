3571e0b32f3a6c54463e257bea26ad0c92c07a61
anyajegorova <anna.jerm@outlook.com>
2023-10-08 23:12:42 +0300

    register page

--- bookHERE/src/views/RegisterPage.jsx
+++ bookHERE/src/views/RegisterPage.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+
 const RegisterPage = () => {
     return (
-        <div>
-            Register Page
+        <div className='mt-4 grow flex items-center justify-around'>
+            <div className='mb-64'>
+                <h1 className='text-4xl text-center mb-4'>Register</h1>
+                <form className='max-w-md mx-auto'>
+                <input type='text' placeholder={'Your name..'} />
+                    <input type='email' placeholder={'Email..'} />
+                    <input type='email' placeholder={'Password..'} />
+                    <button className='primary'>Register</button>
+                    <div className='text-center py-4 text-gray-500'>
+                        Already have an account? <Link className='underline text-black' to={'/login'}>Login</Link>
+                    </div>
+                </form>
+            </div>
+
         </div>
     )
 }
